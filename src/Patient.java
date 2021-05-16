1e0004b73b61127e9e3be2177508a9c2f64153e7
Juan Arango <jblackheart581@gmail.com>
2021-05-16 02:16:52 -0500

    Patien class created

---
+++ src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phioneNumber;
+    String birthday;
+    double weight;
+    double heigh;
+    String blood;
+
+    public Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
